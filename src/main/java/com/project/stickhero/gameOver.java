746e4c73f1ae8f21b3f4ce6abbfe4b04340d7854
vaishviverma <vaishvi22609@iiitd.ac.in>
2023-11-19 03:45:29 +0530

    Added gamePlaying.fxml file

--- src/main/java/com/project/stickhero/gameOver.java
+++ src/main/java/com/project/stickhero/gameOver.java
@@ -2,13 +2,7 @@ package com.project.stickhero;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class gameOver {
 
--- src/main/java/com/project/stickhero/homepage.java
+++ src/main/java/com/project/stickhero/homepage.java
@@ -3,15 +3,11 @@ package com.project.stickhero;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 
 public class homepage {
 
     @FXML
     private Button playbutton;
-
-    @FXML
-    private VBox vboxhomepage;
 
     @FXML
     void onPlayButtonClick(ActionEvent event) {
