75ed7df25781861a5523f191c396125f448fb928
vaishviverma <vaishvi22609@iiitd.ac.in>
2023-11-28 02:16:20 +0530

    updated

--- src/main/java/com/project/stickhero/StickHero.java
+++ src/main/java/com/project/stickhero/StickHero.java
@@ -1,5 +1,5 @@
 package com.project.stickhero;
-
+import javafx.util.Duration;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,16 +8,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
+import javafx.animation.*;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import javafx.scene.image.ImageView;
 
+import java.util.*;
 public class StickHero extends Application {
 
     private Stage stage;
@@ -35,6 +37,9 @@ public class StickHero extends Application {
 
     @FXML
     private Rectangle pillar;
+
+    @FXML
+    private ImageView slime;
 
     @FXML
     void adsButton(ActionEvent event) {
@@ -56,22 +61,40 @@ public class StickHero extends Application {
         stage.show();
 
         Rectangle stick = new Rectangle(5, 0, Color.BLACK);
+
+        Rectangle pil = new Rectangle(8, 257, 200, 140);
+        pil.setFill(Color.BLACK); // Set fill color
+        pil.setStroke(Color.BLACK);
+        ((Pane) root).getChildren().add(pil);
+
+        stick.setLayoutX(pil.getLayoutX()+200);
         ((Pane) root).getChildren().add(stick);
 
-        stick.setLayoutX(100);
-        stick.setLayoutY(100);
+        Random random = new Random();
+        double gen=(double)random.nextInt(10,200);
+        double gen2=(double)random.nextInt(10,200);
+        Rectangle pil2= new Rectangle(gen+pil.getLayoutX()+pil.getWidth(), pil.getY(), gen2, pil.getHeight());
+        pil2.setFill(Color.BLACK); // Set fill color
+        pil2.setStroke(Color.BLACK);
+        ((Pane) root).getChildren().add(pil2);
+
 
         final double[] stickHeight = {0.0};
-        AtomicReference<Double> yCoordinateStick = new AtomicReference<>((double) 200);
+        AtomicReference<Double> yCoordinateStick = new AtomicReference<>((double) pil.getY());
 
         scene.setOnKeyPressed(event1 -> {
             if (event1.getCode() == KeyCode.UP) {
                 stickHeight[0] +=1;
+
                 yCoordinateStick.updateAndGet(v -> new Double((double) (v - 1)));
                 stick.setLayoutY(yCoordinateStick.get());
                 stick.setHeight(stickHeight[0]);
+
             }
         });
+
+
+//
 
 
 
